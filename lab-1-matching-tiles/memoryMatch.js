6fe325c404867319f6ccd397c8b24288a3b0d792
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-06-15 13:16:59 +0100

    reveal function and click event handler

--- lab-1-matching-tiles/memoryMatch.js
+++ lab-1-matching-tiles/memoryMatch.js
@@ -1,5 +1,13 @@
 const randomAnswers = () => [1,1,2,2,3,3,4,4,5].sort((a,b) => 0.5-Math.random());
 
+let clickedArray = [];
+
+const reveal = (cell) => {
+  cell.clicked = true;
+  cell.innerHTML = cell.value;
+  cell.style.backgroundColor = "red";
+};
+
 const setUp = () => {
   const answers = randomAnswers();
   const grid = document.getElementsByTagName("td");
@@ -18,6 +26,12 @@ const setUp = () => {
       function() {
         if (!this.completed && !this.clicked)
           this.style.background = "blue";
+      }
+    );
+    cell.addEventListener("click",
+      function() {
+        clickedArray.push(this);
+        reveal(this);
       }
     );
   };
